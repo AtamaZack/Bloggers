fb563f35258817314b68689ff2b7db3c569ed9dc
ManuelDominic <ematembu2@gmail.com>
2019-01-21 12:17:55 +0300

    MOdify user_model

--- models/user.py
+++ models/user.py
@@ -1,9 +1,8 @@
 class Users():
     user_id=1
 
-    def __init__(self,first_name,last_name,email,password,status):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self,user_name,email,password,status):
+        self.user_name = user_name
         self.email = email
         self.password = password
         self.user_id = Users.user_id
@@ -12,18 +11,20 @@ class Users():
 
 class User(Users):
 
-    def __init__(self,first_name,last_name,email,password,status):
-    Users.__init__(self,first_name,last_name,email,password,status):
+    def __init__(self,user_name,email,password,status):
+    Users.__init__(self,user_name,email,password,status):
         self.status = 'user'
 
 class Moderators(Users):
 
-    def __init__(self,first_name,last_name,email,password,status):
-    Users.__init__(self,first_name,last_name,email,password,status):
+    def __init__(self,user_name,email,password,status):
+    Users.__init__(self,user_name,email,password,status):
         self.status = 'moderator'
 
 class Admin(Users):
 
-    def __init__(self,first_name,last_name,email,password,status):
-    Users.__init__(self,first_name,last_name,email,password,status):
+    def __init__(self,user_name,email,password,status):
+    Users.__init__(self,user_name,email,password,status):
         self.status = 'admin'
+
+
