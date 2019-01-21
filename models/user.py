b28fe05c5a84eb23caf9fbd80e91fab88cfc596a
Okiror Frank <okirorfrank3@gmail.com>
2019-01-21 13:09:23 +0300

    Update user.py
    update login

--- models/user.py
+++ models/user.py
@@ -1,15 +1,5 @@
-def login(self,email, password, accounts):
-    email = input("Please enter your email:\n")
-    password = input("Please enter your password:\n")
-
-    for user in accounts:
-        if user["email"] == email and password["password"] == password:
-            if user["status"] == "normal_user":
-                print("user successfully logged in!")
-            if user["status"] == "moderate_user":
-                print("user successfully logged in!")
-            else:
-                print("Admin successfully logged in!")
-        else:
-            print("invalid login credentials")
-login(users_list)
+def login(email, password):
+  for user in accounts:
+    if user['email'] == email and user['password'] == password:
+      return True
+  return False
