8cf5f2171b3988cfe305f69332b7b1fb9bbd4db0
nadralia <nadralia@gmail.com>
2019-01-21 12:03:10 +0300

    [Delivers #163356361] feature comment here i wrote a class for comments which contains add , delete and edit

--- models/comment.py
+++ models/comment.py
@@ -1,12 +1,12 @@
-comments = []
-class Comment:
+class Comments:
     def __init__(self,comment_id,comment_title,comment_body,createdby):
         self.comment_id = comment_id
         self.comment_title = comment_title
         self.comment_body = comment_body
         self.createdby = createdby
+        self.all_comments= []
 
-    def create_comment(self):
+    def get_comment(self):
         return {
             "comment_id": self.comment_id,
             "comment_title": self.comment_title,
@@ -15,14 +15,19 @@ class Comment:
             }
 
     def delete_comment(self, comment_id):
-        if len(comments) > 0:
+        if len(self.all_comments) > 0:
             try:
-                returned_comment = [comment for comment in comments if comment["comment_id"] == int(comment_id)]
-                comments.remove(returned_comment[0])
-                return comments
+                returned_comment = [comment for comment in self.all_comments if comment["comment_id"] == int(comment_id)]
+                self.all_comments.remove(returned_comment[0])
+                return self.all_comments
             except Exception as error:
                 return error   
         return False
 
-    def edit_comment(self):
-        pass
+    def edit_comment(self, comment_id,comment_title,comment_body,createdby ):
+        returned_comment = [comment for comment in self.all_comments if comment["comment_id"] == int(comment_id)]
+        returned_comment[0]['comment_title'] = comment_title
+        returned_comment[0]['comment_body'] = comment_body
+        returned_comment[0]['createdby'] = createdby
+        return True
+
